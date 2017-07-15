2564159a206967ee2da9913382796b9bfcec445f
neoalitcahya <neo.alit.cahya@live.com>
2017-07-15 17:07:42 +0700

    1. change back button on profileTeman 2. add edit post, edit comment, delete post on profile 3. add view my profile

--- js/client-profilTeman.js
+++ js/client-profilTeman.js
@@ -35,8 +35,8 @@ function gotoProfilTeman(clickedId){
 			}
 			else
 			{
+				//ini buka profile akun sendiri
 				getAllTemanPost(id_teman);
-				//console.log("teman");
 				getProfilTeman(id_teman,1);
 			}
 		}
@@ -49,9 +49,10 @@ function gotoProfilTeman(clickedId){
 }
 
 function getAllTemanPost(clickedId) {
+	
+	console.log("masuk1");
 	var id_teman = clickedId;
 	var link=urlnya+'/api/post/getAllPostByUser?id_user='+id_teman;
-
 		$.ajax({ dataType: "jsonp",
 		    url: link,
 		    type: 'GET',
@@ -101,6 +102,11 @@ function getAllTemanPost(clickedId) {
 					html += 					"<img src='data:image/jpeg;base64,"+z[i]['user_foto']+"' class='profilePicture' style='padding:0px; margin-right:-20px; margin-bottom:-10px; position:relative; top:-5px;' width='30'>";
 					html += 				"</td>";
 					html += 				"<td style='font-weight:bold;'>"+z[i]['nama']+"</td>";
+					if(z[i]['nama']==getData("active_user_nama"))
+					{
+						html += 				"<td style='font-weight:bold;'><i onclick='editPostProfile(this.id)' id='"+z[i]['id']+"' class='fa fa-caret-square-o-down' aria-hidden='true'></i></td>";
+						html += 				"<td style='font-weight:bold;'><i onclick='pilihanHapusDataProfile(this.id)' id='"+z[i]['id']+"' class='fa fa-minus' aria-hidden='true'></i></td>";
+					}
 					html += 			"</tr>";
 					html += 			"<tr>";
 					html += 				"<td style='font-size:10px;'>"+z[i]['created_at']+"</td>";
@@ -132,6 +138,11 @@ function getAllTemanPost(clickedId) {
 					html += 					"<img src='data:image/jpeg;base64,"+z[i]['user_foto']+"' class='profilePicture' style='padding:0px; margin-right:-20px; margin-bottom:-10px; position:relative; top:-5px;' width='30'>";
 					html += 				"</td>";
 					html += 				"<td style='font-weight:bold;'>"+z[i]['nama']+"</td>";
+					if(z[i]['nama']==getData("active_user_nama"))
+					{
+						html += 				"<td style='font-weight:bold;'><i onclick='editPostProfile(this.id)' id='"+z[i]['id']+"' class='fa fa-caret-square-o-down' aria-hidden='true'></i></td>";
+						html += 				"<td style='font-weight:bold;'><i onclick='pilihanHapusDataProfile(this.id)' id='"+z[i]['id']+"' class='fa fa-minus' aria-hidden='true'></i></td>";
+					}
 					html += 			"</tr>";
 					html += 			"<tr>";
 					html += 				"<td style='font-size:10px;'>"+z[i]['created_at']+"</td>";
@@ -158,7 +169,7 @@ function getAllTemanPost(clickedId) {
 function getAllTemanPost(clickedId, id_post) {
 	var id_teman = clickedId;
 	var link=urlnya+'/api/post/getAllPostByUser?id_user='+id_teman;
-
+	console.log(link);
 		$.ajax({ dataType: "jsonp",
 		    url: link,
 		    type: 'GET',
@@ -208,6 +219,11 @@ function getAllTemanPost(clickedId, id_post) {
 					html += 					"<img src='data:image/jpeg;base64,"+z[i]['user_foto']+"' class='profilePicture' style='padding:0px; margin-right:-20px; margin-bottom:-10px; position:relative; top:-5px;' width='30'>";
 					html += 				"</td>";
 					html += 				"<td style='font-weight:bold;'>"+z[i]['nama']+"</td>";
+					if(z[i]['nama']==getData("active_user_nama"))
+					{
+						html += 				"<td style='font-weight:bold;'><i onclick='editPostProfile(this.id)' id='"+z[i]['id']+"' class='fa fa-caret-square-o-down' aria-hidden='true'></i></td>";
+						html += 				"<td style='font-weight:bold;'><i onclick='pilihanHapusDataProfile(this.id)' id='"+z[i]['id']+"' class='fa fa-minus' aria-hidden='true'></i></td>";
+					}
 					html += 			"</tr>";
 					html += 			"<tr>";
 					html += 				"<td style='font-size:10px;'>"+z[i]['created_at']+"</td>";
@@ -239,6 +255,11 @@ function getAllTemanPost(clickedId, id_post) {
 					html += 					"<img src='data:image/jpeg;base64,"+z[i]['user_foto']+"' class='profilePicture' style='padding:0px; margin-right:-20px; margin-bottom:-10px; position:relative; top:-5px;' width='30'>";
 					html += 				"</td>";
 					html += 				"<td style='font-weight:bold;'>"+z[i]['nama']+"</td>";
+					if(z[i]['nama']==getData("active_user_nama"))
+					{
+						html += 				"<td style='font-weight:bold;'><i onclick='editPostProfile(this.id)' id='"+z[i]['id']+"' class='fa fa-caret-square-o-down' aria-hidden='true'></i></td>";
+						html += 				"<td style='font-weight:bold;'><i onclick='pilihanHapusDataProfile(this.id)' id='"+z[i]['id']+"' class='fa fa-minus' aria-hidden='true'></i></td>";
+					}
 					html += 			"</tr>";
 					html += 			"<tr>";
 					html += 				"<td style='font-size:10px;'>"+z[i]['created_at']+"</td>";
@@ -426,6 +447,10 @@ function bacaTemanKomentar(clicked_id) {
 							html += 				"</td>";
 							html += 				"<td style='font-weight:bold;'>"+z[i]['nama']+"</td>";
 							html += 				"<td style='font-size:10px;'>"+z[i]['deskripsi']+"</td>";
+							if(z[i]['nama']==getData("active_user_nama"))
+							{
+								html += 				"<td style='font-weight:bold;'><i onclick='editKomentarKuProfile("+clicked_id+",this.id)' id='"+z[i]['id']+"' class='fa fa-caret-square-o-down' aria-hidden='true'></i></td>";
+							}
 							html += 			"</tr>";
 							html += 			"<tr>";
 							html += 				"<td style='font-size:10px;'>"+z[i]['created_at']+"</td>";
@@ -581,4 +606,241 @@ function addFriend(clickedId){
 		}).fail(function(x){
 			myApp.alert("Tambah teman gagal", 'Perhatian!');
 		}); 
+}
+
+function editPostProfile(clicked_id)
+{
+	var id_user = getData("active_user_id");
+	var formData=JSON.stringify({
+						id_user:id_user,
+					});
+	var link=urlnya+'/api/post/getAllPostFriendByUser?id_user='+id_user;
+
+	$.ajax({
+	    url: link,
+	    type: 'GET',
+	    contentType: false,
+	    processData: false
+	}).done(function(z){
+		var coba="";
+		var dataLength=0;
+		for (var ii = 0 ; ii < z.length ; ii++) {
+			coba+=z[ii]['id']+"|"; 
+			dataLength++;
+		}
+		for(var i=0;i<dataLength;i++)
+		{
+			if(clicked_id==z[i]['id'])
+			{
+				if(z[i]['foto']!="")
+				{
+					myApp.popup('.popup-edit-profileTeman');
+					var popupHTML=	'<div class="popup">'+
+								'<div class="content-block">'+
+								'<p>Edit Kiriman</p>'+
+											'<div class="page-content">'+
+											'<center><textarea id="statusEdit" style="resize:none; margin-top:10px; width:90%; height:60px;" '+
+											'placeholder="Tulis Status Anda..">'+z[i]['deskripsi']+'</textarea>'+
+											"<img src='data:image/jpeg;base64,"+z[i]['foto']+"' style='width:100%; height:100%;'>"+
+											'</center>'+
+										'<div style="height:0px;overflow:hidden">'+
+										'<input type="file" id="file_editProfile" accept="image/*"/>'+
+										'</div>'+
+										'<p><a href="#" class="button active" onclick="statusEditPostProfile(this.id);" id='+clicked_id+' type="submit" style="width:70px; float:right; margin-right:5%;">Update</a></p>'+
+										'<p><a href="#" class="button"  onclick="chooseFile_editProfile();" style=" float:right; margin-right:10px; width:85px;">Gambar..</a></p>'+
+							   ' </div>'+
+							   '<p><a href="#" onclick="tutupModalProfile()" class="close-popup">Kembali</a></p>'+
+						'</div>'+
+					'</div>';
+					myApp.popup(popupHTML);
+				}
+				else
+				{
+					myApp.popup('.popup-edit-profileTeman');
+					var popupHTML=	'<div class="popup">'+
+								'<div class="content-block">'+
+								'<p>Edit Kiriman</p>'+
+											'<div class="page-content">'+
+											'<center><textarea id="statusEdit" style="resize:none; margin-top:10px; width:90%; height:60px;" '+
+											'placeholder="Tulis Status Anda..">'+z[i]['deskripsi']+'</textarea>'+
+											'</center>'+
+										'<div style="height:0px;overflow:hidden">'+
+										'<input type="file" id="file_editProfile" accept="image/*"/>'+
+										'</div>'+
+										'<p><a href="#" class="button active" onclick="statusEditPostProfile(this.id);" id='+clicked_id+' type="submit" style="width:70px; float:right; margin-right:5%;">Update</a></p>'+
+										'<p><a href="#" class="button"  onclick="chooseFile_editProfile();" style=" float:right; margin-right:10px; width:85px;">Gambar..</a></p>'+
+							   ' </div>'+
+							   '<p><a href="#" onclick="tutupModalProfile()" class="close-popup">Kembali</a></p>'+
+						'</div>'+
+					'</div>';
+					myApp.popup(popupHTML);
+				}
+			}
+		}
+	}).fail(function(x){
+		myApp.alert("Pengambilan status user gagal", 'Perhatian!');
+	}); 
+}
+
+function pilihanHapusDataProfile(clicked_id){
+  myApp.modal({
+    title:  'Pilihan',
+    text: 'Apakah anda ingin menghapus kiriman ini?',
+    buttons: [
+      {
+        text: 'Tidak',
+        onClick: function() {
+          //myApp.alert('You clicked first button!')
+        }
+      },
+      {
+        text: 'Ya',
+		bold: true,
+        onClick: function() {
+			hapusDataProfile(clicked_id);
+        }
+      },
+    ]
+  })
+}
+
+function hapusDataProfile(clicked_id)
+{
+	var link=urlnya+'/api/post/deletePost?id_post='+clicked_id;
+	$.ajax({
+		    url: link,
+		    type: 'GET',
+		    contentType: false,
+		    processData: false
+		}).done(function(z){
+			getAllTemanPost(clicked_id);
+		}).fail(function(x){
+			myApp.alert('Maaf tidak dapat menghapus kiriman, silahkan coba lagi', 'Perhatian!');
+			var coba="";
+			for (var ii = 0 ; ii < formData.entries().length; ii++) {
+				coba+=formData.entries()[ii][0]+ ', '; 
+			}
+			console.log(coba);
+		});
+}
+
+function statusEditPostProfile(clicked_id) {
+	//ON PROGRESS
+	var id_user = getData("active_user_id");
+	var status = document.getElementById("statusEdit").value;
+	
+	var link=urlnya+'/api/post/updatePost/';
+	
+	if(status=="")
+	{
+		myApp.alert('Status tidak boleh kosong', 'Perhatian!');
+	}
+	else
+	{
+		var blob=$("#file_editProfile")[0].files[0];
+		var formData = new FormData();
+		formData.append("id_user", id_user);
+		formData.append("deskripsi", status);
+		formData.append("id_post", clicked_id);
+		formData.append("file", blob);
+
+		$.ajax({
+		    url: link,
+		    data: formData,
+		    type: 'POST',
+		    contentType: false,
+		    processData: false
+		}).done(function(z){
+			myApp.closeModal();
+			getAllTemanPost(id_user,null);
+		}).fail(function(x){
+			myApp.alert('Maaf tidak dapat menambah status, silahkan coba lagi', 'Perhatian!');
+			var coba="";
+			for (var ii = 0 ; ii < formData.entries().length; ii++) {
+				coba+=formData.entries()[ii][0]+ ', '; 
+			}
+			console.log(coba);
+		});
+		
+	}
+}
+
+function tutupModalProfile() {
+	getAllTemanPost(getData("active_user_id"),null);
+}
+
+function editKomentarKuProfile(id_post,clicked_id)
+{
+	var id_user = getData("active_user_id");
+	var id_komentar = clicked_id;
+	
+	$(document).ready(function(){
+			var link=urlnya+'/api/komentar?id_post='+id_post;
+				
+			$.ajax({ dataType: "jsonp",
+				url: link,
+				type: 'GET',
+				contentType: false,
+				processData: false
+			}).done(function(z){
+				if(z.length>0)
+				{
+					for(var i=0;i<z.length;i++)
+					{
+						if(clicked_id==z[i]['id'])
+						{
+							myApp.popup('.popup-edit-profileTeman');
+								var popupHTML=	'<div class="popup">'+
+											'<div class="content-block">'+
+											'<p>Edit Kiriman</p>'+
+														'<div class="page-content">'+
+														'<center><textarea id="komentarEdit" style="resize:none; margin-top:10px; width:90%; height:60px;" '+
+														'placeholder="Tulis Komentar Anda..">'+z[i]['deskripsi']+'</textarea>'+
+														'</center>'+
+													'<div style="height:0px;overflow:hidden">'+
+													'</div>'+
+													'<p><a href="#" class="button active close-popup" onclick="simpanKomentarProfile('+id_post+',this.id);" id='+clicked_id+' type="submit" style="width:70px; float:right; margin-right:5%;">Update</a></p>'+
+										   ' </div>'+
+										   '<p><a href="#" class="close-popup">Kembali</a></p>'+
+									'</div>'+
+								'</div>';
+								myApp.popup(popupHTML);
+						}
+					}
+				}
+			}).fail(function(x){
+				myApp.alert('Maaf tidak dapat mengomentari status, silahkan coba lagi', 'Perhatian!');
+			});
+			
+		});
+}
+
+function simpanKomentarProfile(id_post, clicked_id)
+{
+	var id_user = getData("active_user_id");
+	var id_komentar = clicked_id;
+	var deskripsi=document.getElementById("komentarEdit").value;
+	
+	
+	var formData = JSON.stringify({
+					id_user:id_user,
+					id_komentar:id_komentar,
+					deskripsi:deskripsi
+				});
+	
+	var link=urlnya+'/api/komentar/updateKomentar/';
+	
+	$.ajax({
+	    url: link,
+	    data: formData,
+	    type: 'POST',
+	    contentType: false,
+	    processData: false
+	}).done(function(z){
+		myApp.closeModal();
+		bacaTemanKomentar(id_post);
+		bacaTemanKomentar(id_post);
+	}).fail(function(x){
+		myApp.alert('Maaf terjadi kesalahan, silahkan coba lagi', 'Perhatian!');
+	});
 }
